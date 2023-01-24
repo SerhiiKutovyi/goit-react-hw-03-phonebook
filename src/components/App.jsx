72a9379ccf59b5eefff1cdb6c632d07a4e3d8f99
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-24 12:52:55 +0200

    Start HW-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,95 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import styled from 'styled-components';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm ';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const newUser = {
+      id: nanoid(),
+      ...data,
+    };
+    if (this.state.contacts.some(ele => ele.name === data.name)) {
+      alert(`${data.name} is already in contacts!`);
+      return;
+    } else {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, newUser],
+        };
+      });
+      return;
+    }
+  };
+
+  filterUsers = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  verification = () => {
+    if (!this.state.filter) {
+      return this.state.contacts;
+    } else {
+      return this.state.contacts.filter(
+        user =>
+          user.name.includes(this.state.filter) ||
+          user.number.includes(this.state.filter)
+      );
+    }
+  };
+
+  deleteUsers = userId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(user => user.id !== userId),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Section>
+          <h1>Phonebook</h1>
+          <ContactForm onSubmit={this.formSubmitHandler} />
+          <h2>Contacts</h2>
+          <Filter filter={this.state.filter} click={this.filterUsers} />
+
+          <ContactList
+            contacts={this.verification()}
+            deleteUsers={this.deleteUsers}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 40px;
+
+  h1 {
+    margin-bottom: 40px;
+
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 72px;
+
+    color: #000000;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+  }
+`;
---
+++ src/components/ContactList/ContactList.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ContactStyled = styled.ul`
+  margin-left: 40px;
+  li {
+    display: flex;
+    margin-bottom: 10px;
+  }
+
+  p {
+    width: 300px;
+  }
+
+  button {
+    background-color: coral;
+    color: white;
+  }
+`;
